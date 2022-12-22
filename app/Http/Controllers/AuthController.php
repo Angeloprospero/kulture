9c26dc065b386a0516661d3f06274ebf728ad0fe
Michael Vigara <ryuusenken@gmail.com>
2022-12-22 15:44:57 +0100

    Add existing file

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Auth;
+use Illuminate\Validation\ValidationException;
 use validator;
 use App\Models\user;
 use PharIo\Manifest\Author;
@@ -38,8 +39,38 @@ class AuthController extends Controller
             'email' => 'required|email',
             'password' => 'required|alphaNum|min:3'
         ]);
+        /*  //attempt to login
+        if(FacadesAuth::attempt(['email' => $request->email, 'password' => $request->password])){
+        //if successful, return the authenticated user
+        return response()->json(FacadesAuth::user());
+        }
+        //if not successful, return an error
+        return response()->json(['error' => 'Unable to login with provided credentials'], 401);
+        }*/
+       
         $user = user::where('email', $request->email)->first();
-        if(!$u)
+        if (!$user)
+            return "doesnt exist";
+        if (!Hash::check($request->password, $user->password))
+            ;
+        if (!$user)
+            return "do not exist";
+        if (!Hash::check($request-> password, $user->password)) {
+            throw ValidationException::withMessages(["message" => "wrong details"]);
+        }
+
+
+
+        function logout(Request $request)
+        {
+            FacadesAuth::logout();
+            return response()->json('loggedout');
+        }
+
+    }
+
+
+        /*
         $user_data = array([
                 'email' => $request->get('email'),
                 'password' => $request->get('password')
@@ -63,12 +94,8 @@ class AuthController extends Controller
      return response ()->json(['message'=>'login' ],200);
     }
 
-    function logout()
-    {
-     FacadesAuth::logout();
-     return response()->json('loggedout');
-    }
+    
         
     
-     
+ */    
 }
