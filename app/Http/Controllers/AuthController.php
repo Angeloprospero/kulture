efa8a38d0fe362da881a9ad04e39006e701b9be8
Michael Vigara <ryuusenken@gmail.com>
2022-12-22 10:28:18 +0100

    Add existing file

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use App\Models\user;
+use Auth;
+use Illu
+use validator;
 class AuthController extends Controller
 {
     //
@@ -27,5 +30,38 @@ class AuthController extends Controller
         $user->save();
         $token = rand(000, 999);
         return response ()->json(['message'=>'registered', $user, $token ],200);
+    }
+    function checklogin(Request $request)
+    {
+     $this->validate($request, [
+      'email'   => 'required|email',
+      'password'  => 'required|alphaNum|min:3'
+     ]);
+
+     $user_data = array(
+      'email'  => $request->get('email'),
+      'password' => $request->get('password')
+     );
+
+     if(Auth::attempt($user_data))
+     {
+      return redirect('main/successlogin');
+     }
+     else
+     {
+      return back()->with('error', 'Wrong Login Details');
+     }
+
+    }
+
+    function successlogin()
+    {
+     return view('successlogin');
+    }
+
+    function logout()
+    {
+     Auth::logout();
+     return redirect('main');
     }
 }
